15b0c44a1bc3d1aa72320bf2eea8b13bd2438cae
Harry Kavalikas <harry.kavalikas@hotmail.com>
2026-02-11 07:12:18 +0000

    feat: replace icon fonts with SVGs and optimize font loading in HTML

--- src/HomePage.tsx
+++ src/HomePage.tsx
@@ -181,7 +181,7 @@ function HomePage() {
               aria-disabled="true"
               title="iOS app is not available yet."
             >
-              <i className="ti ti-brand-apple store-icon-font" aria-hidden="true" />
+              <svg className="store-icon-font" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M8.286 7.008c-3.216 0 -4.286 3.23 -4.286 5.92c0 3.229 2.143 8.072 4.286 8.072c1.165 -.05 1.799 -.538 3.214 -.538c1.406 0 1.607 .538 3.214 .538s4.286 -3.229 4.286 -5.381c-.03 -.011 -2.649 -.434 -2.679 -3.23c-.02 -2.335 2.589 -3.179 2.679 -3.228c-1.096 -1.606 -3.162 -2.113 -3.75 -2.153c-1.535 -.12 -3.032 1.077 -3.75 1.077c-.729 0 -2.036 -1.077 -3.214 -1.077" /><path d="M12 4a2 2 0 0 0 2 -2a2 2 0 0 0 -2 2" /></svg>
               <span className="store-badge-copy">
                 <small>iOS</small>
                 <strong>App Store</strong>
@@ -195,10 +195,7 @@ function HomePage() {
               aria-disabled="true"
               title="Play Store listing will be available when the app goes live."
             >
-              <i
-                className="ti ti-brand-google-play store-icon-font"
-                aria-hidden="true"
-              />
+              <svg className="store-icon-font" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M4 3.71v16.58a.7 .7 0 0 0 1.05 .606l14.622 -8.42a.55 .55 0 0 0 0 -.953l-14.622 -8.419a.7 .7 0 0 0 -1.05 .607l0 -.001" /><path d="M15 9l-10.5 11.5" /><path d="M4.5 3.5l10.5 11.5" /></svg>
               <span className="store-badge-copy">
                 <small>Android</small>
                 <strong>Google Play</strong>
